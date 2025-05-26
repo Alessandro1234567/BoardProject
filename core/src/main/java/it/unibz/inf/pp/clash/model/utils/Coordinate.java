60bdf9322770e4ddcb09bf9a9eb4ec8d509db902
cennicola6 <nicchen@unibz.it>
2025-05-26 18:31:25 +0200

    kept only optToUnit method

--- core/src/main/java/it/unibz/inf/pp/clash/model/utils/Coordinate.java
+++ core/src/main/java/it/unibz/inf/pp/clash/model/utils/Coordinate.java
@@ -1,4 +1,19 @@
 package it.unibz.inf.pp.clash.model.utils;
 
 public class Coordinate {
+    public int x, y;
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
+
